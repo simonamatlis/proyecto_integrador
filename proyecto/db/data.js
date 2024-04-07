9489d2f02564be4855f8914f180776b6d44ad618
julietaBdenevi <jburgosdenevi@udesa.edu.ar>
2024-04-07 01:36:48 -0300

    base modulo de data

---
+++ proyecto/db/data.js
@@ -0,0 +1,16 @@
+/** contendrá un objeto literal con los datos de un usuario. Los datos deben coincidir con la información pedida en el formulario de registro. 
+Los datos del usuario deben mostrarse en todos los lugares donde aparezca el header logueado y en “mi perfil”.
+ */
+
+const Data = {
+
+    lista: [
+        {
+            Email:"",
+            userName:"",
+            Password:"" ,
+            bday:"",
+            id:"",
+            profilePic: ""},
+    ]
+}
