bcd0e79d7173c046c14d8dfca8f33bd4f808c67f
julietaBdenevi <jburgosdenevi@udesa.edu.ar>
2024-04-07 01:41:05 -0300

    mismo commit

--- proyecto/db/data.js
+++ proyecto/db/data.js
@@ -4,7 +4,7 @@ Los datos del usuario deben mostrarse en todos los lugares donde aparezca el hea
 
 const Data = {
 
-    lista: [
+    usuario: [
         {
             Email:"",
             userName:"",
@@ -12,5 +12,19 @@ const Data = {
             bday:"",
             id:"",
             profilePic: ""},
+    ],
+
+    producto: [
+        {
+            /** array de objetos literales con los datos de los productos. Debe tener al menos 10 productos. Los datos deben coincidir con la información pedida en el formulario de carga de productos. 
+La información de los productos debe mostrarse en todos los lugares de la maqueta en donde aparezca un listado de productos por ejemplo en home y en  “mi perfil”.
+comentarios: el objeto literal de productos tendrá una propiedad lladma "comentarios". Será un array de objetos literales con los datos de los comentarios que se ven en el detalle de un producto.. La información de cada comentario debe ser:
+nombre de usuario.
+texto del comentario.
+imagen de perfil: link dentro del proyecto con la ruta a una imagen de perfil. ¿Recordás en qué parte del proyecto van estas imágenes? 
+ */
+        }
     ]
+
+
 }
