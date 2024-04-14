e7ce2466010ba969ccdc123411f6a4ccd9fe01fd
julietaBdenevi <jburgosdenevi@udesa.edu.ar>
2024-04-14 19:37:52 -0300

    TERMINE punto 2

--- proyecto/db/baseDeDatos.sql
+++ proyecto/db/baseDeDatos.sql
@@ -1,1 +1,117 @@
-add
+CREATE schema proyectoInt2; 
+USE proyectoInt2;
+
+--  TABLA DE USUARIOS--
+CREATE TABLE usuario (
+id_usuario INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
+email TEXT NOT NULL,
+contra TEXT NOT NULL,
+fecha DATE NOT NULL,
+dni INT NOT NULL,
+createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+deletedAt  TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP 
+);
+
+--  TABLA DE PRODUCTOS--
+CREATE TABLE productos (
+id_producto INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
+user_id INT UNSIGNED,
+nombre_imagen TEXT NOT NULL,
+nombre_producto TEXT NOT NULL,
+descripción_producto TEXT NOT NULL,
+createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+deletedAt  TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+FOREIGN KEY (user_id) REFERENCES usuario(id_usuario)
+);
+
+--  TABLA DE COMENTARIOS--
+CREATE TABLE comentarios (
+id_comentario INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
+user_id INT UNSIGNED,
+producto_id INT UNSIGNED,
+comentario TEXT NOT NULL,
+createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updatedAt  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+deletedAt  TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+FOREIGN KEY (user_id) REFERENCES usuario(id_usuario),
+FOREIGN KEY (producto_id) REFERENCES productos(id_producto)
+);
+
+--  VALUES DE LOS 5 USUARIOS --
+INSERT INTO usuario (id_usuario,email,contra,fecha, dni,createdAt,deletedAt)
+VALUES(default, 'luciana@gmail.com', 'luCHi', '2024-04-08', 45891234,null, null);
+
+INSERT INTO usuario
+VALUES(default, 'juana@gmail.com', 'Juana', '2024-03-08', 46791334,null, null);
+
+INSERT INTO usuario
+VALUES(default, 'lara@gmail.com', 'lara', '2024-03-10', 42450334,null, null);
+
+INSERT INTO usuario
+VALUES(default, 'matias@gmail.com', 'Matias', '2024-02-25', 46707834,null, null);
+
+INSERT INTO usuario
+VALUES(default, 'ana@gmail.com', 'ana', '2024-04-03', 42791344,null, null);
+
+
+
+--  VALUES DE LOS 10 PRODUCTOS--
+use proyectoInt2;
+INSERT INTO productos (id_producto, user_id, nombre_imagen, nombre_producto, descripción_producto, createdAt, updatedAt,deletedAt)
+VALUES                (default, 1,'images/products/remera.png', 'remera', 'Remera escote recto y hombros semicubiertos.', null, null, null),
+					  (default, 2, 'pantalón', 'images/products/pantalon.png','Pantalón de tiro alto y cintura con trabillas. Pierna recta. ', null, null, null),
+					  (default, 3, 'jean', 'images/products/jean.png','Jean Slim Fit - Skinny Leg - Tiro Alto', null, null, null),
+				      (default, 4, 'top', 'images/products/top.png','Top corto de escote pico cruzado y manga sisa.', null, null, null),
+					  (default, 5, 'campera', 'images/products/campera.png','Cazadora corta de cuello solapa con manga larga', null, null, null),
+					  (default, 1, 'vestido', 'images/products/vestido.png', 'Vestido corto de escote recto y hombros descubiertos.', null, null, null),
+	                  (default, 2, 'zapatos', 'images/products/zapatos.png','Zapato destalonado de tacón kitten. Tira trasera con cierre mediante hebilla. Acabado en punta.', null, null, null),
+			          (default, 3, 'pollera', 'images/products/pollera.png','Falda pantalón de tiro alto.', null, null, null),
+	                  (default, 4, 'blazer', 'images/products/blazer.png','Blazer abierta de cuello y solapa con manga larga acabada en vuelta y hombreras.', null, null, null),
+					  (default, 5, 'mono', 'images/products/mono.png', 'Mono largo de cuello redondo con manga sisa y manga corta. ', null, null, null);
+
+
+--  VALUES DE LOS 30 COMENTARIOS--
+use proyectoInt2;
+INSERT INTO comentarios (id_comentario, user_id, producto_id, comentario, createdAt,updatedAt, deletedAt)
+VALUES 					(default, 1, 1, 'Muy linda remera', null, null, null),
+						(default, 2, 1, 'Los brillos que tiene es un detalle muy lindo', null, null, null),
+						(default, 3, 1, 'Me encanta como queda puesta!', null, null, null),
+                        
+						(default, 4, 2, 'Muy tiro alto el pantalón para mi gusto', null, null, null),
+						(default, 5, 2, 'Un pantalón muy elegante. Me gusta!', null, null, null),
+						(default, 1, 2, 'Se lo vi a una chica y corrí a comparlo!!', null, null, null),
+                        
+						(default, 2, 3, 'El Jean muy lindo puedo', null, null, null),
+						(default, 3, 3, 'Me encanta, mi jean favorito', null, null, null),
+                        (default, 4, 3, 'Mi hermana me vió el jean y fue a comprarse el mismo ',  null, null, null),
+						
+                        (default, 5, 4, 'El top queda hermoso', null, null, null),
+						(default, 1, 4, 'Muy elegante para salir', null, null, null),
+						(default, 2, 4, 'Me gusta!', null, null, null),
+						
+                        (default, 3, 5, 'La campera que compré es increíblemente abrigada y cómoda! ', null, null, null),
+						(default, 4, 5, 'Perfecta para los días fríos de invierno.', null, null, null),
+						(default, 5, 5, '¡Qué buena calidad tiene la campera que elegí! Definitivamente vale la pena.', null, null, null),
+						
+                        (default, 1, 6, 'Mi nuevo vestido es elegante y me queda genial, estoy muy contenta con la compra.', null, null, null),
+						(default, 2, 6, 'El vestido que compré tiene un color precioso, me hace sentir muy femenina.', null, null, null),
+						(default, 3, 6, 'Qué cómodo es mi vestido nuevo! Ideal para eventos formales y muy favorecedor.', null, null, null),
+						
+                        (default, 4, 7, 'Mis nuevos zapatos son muy cómodos, puedo usarlos todo el día sin problemas.', null, null, null),
+						(default, 5, 7, 'El diseño de mis zapatos nuevos es muy moderno, estoy muy feliz con la elección.', null, null, null),
+                        (default, 1, 7, 'Qué buen material tienen mis zapatos nuevos, se nota la calidad en cada paso.', null, null, null),
+						
+                        (default, 2, 8, 'La pollera que compré es muy versátil, la puedo combinar de muchas formas.', null, null, null),
+						(default, 3, 8, 'Me gusta mucho el color de mi nueva pollera, es perfecto para el verano.', null, null, null),
+						(default, 4, 8, 'Qué buen ajuste tiene mi pollera nueva, es cómoda y favorecedora.', null, null, null),
+						
+                        (default, 5, 9, 'Mi nuevo blazer es elegante y perfecto para ocasiones formales, estoy encantada.', null, null, null),
+						(default, 1, 9, 'Qué buen corte tiene mi blazer nuevo, se ajusta muy bien a mi figura.', null, null, null),
+						(default, 2, 9, 'La calidad de mi blazer nuevo es excelente, se nota que está bien hecho.', null, null, null),
+						
+                        (default, 3, 10, 'El enterito que compré es muy versátil, puedo usarlo en muchas ocasiones diferentes.', null, null, null),
+						(default, 4, 10, 'Me gusta mucho mi enterito nuevo, es muy llamativo.', null, null, null),
+						(default, 5, 10, 'Qué cómodo es mi enterito nuevo, es perfecto para estar cómoda y a la moda.', null, null, null);
+
+
--- proyecto/db/data.js
+++ proyecto/db/data.js
@@ -4,27 +4,270 @@ Los datos del usuario deben mostrarse en todos los lugares donde aparezca el hea
 
 const Data = {
 
-    usuario: [
-        {
-            Email:"",
-            userName:"",
-            Password:"" ,
-            bday:"",
-            id:"",
-            profilePic: ""},
-    ],
+    usuario:  {
+            Email: 'luciana@gmail.com',
+            userName:"lucianaPerez",
+            Password:"luCHi" ,
+            bday:"2024-04-08",
+            dni:"45891234",
+            profilePic: "/images/users/profilepic.avif"},
+    
 
     producto: [
         {
-            /** array de objetos literales con los datos de los productos. Debe tener al menos 10 productos. Los datos deben coincidir con la información pedida en el formulario de carga de productos. 
-La información de los productos debe mostrarse en todos los lugares de la maqueta en donde aparezca un listado de productos por ejemplo en home y en  “mi perfil”.
-comentarios: el objeto literal de productos tendrá una propiedad lladma "comentarios". Será un array de objetos literales con los datos de los comentarios que se ven en el detalle de un producto.. La información de cada comentario debe ser:
-nombre de usuario.
-texto del comentario.
-imagen de perfil: link dentro del proyecto con la ruta a una imagen de perfil. ¿Recordás en qué parte del proyecto van estas imágenes? 
- */
+            Imagen: 'images/products/remera.jpng',
+            nombre: 'remera',
+            description: 'Remera escote recto y hombros semicubiertos.',
+            comentarios: [
+                {
+                    nombreUsuario: 'Luciana',
+                    comentario: 'Muy linda remera',
+                    imagen: '/images/users/1.webp',
+
+                },
+                {
+                    nombreUsuario: 'Juana',
+                    comentario: 'Los brillos que tiene es un detalle muy lindo',
+                    imagen: '/images/users/2.jpeg',
+                },
+                {
+                    nombreUsuario: 'Lara',
+                    comentario: 'Me encanta como queda puesta',
+                    imagen: '/images/users/3.jpeg',
+
+                }
+            ]
+
+        },
+        {
+            Imagen: '/images/products/pantalon.jpeg',
+            nombre: 'pantalon',
+            description: 'Pantalón de tiro alto y cintura con trabillas. Pierna recta.',
+            comentarios: [
+                {
+                    nombreUsuario: 'Matias',
+                    comentario: 'Un pantalón muy elegante. Me gusta!',
+                    imagen: '/images/users/4.avif',
+
+                },
+                {
+                    nombreUsuario: 'Ana',
+                    comentario: 'Muy tiro alto el pantalón para mi gusto',
+                    imagen: '/images/users/5.jpeg',
+                },
+                {
+                    nombreUsuario: 'Luciana',
+                    comentario: 'Se lo vi a una chica y corrí a comparlo!!',
+                    imagen: '/images/users/1.webp',
+
+                }
+            ]
+
+        },
+        {
+            Imagen: 'proyecto_integrador/proyecto/public/images/products/JeanAzul.jpeg',
+            nombre: 'Jean',
+            description: 'Jean Slim Fit - Skinny Leg - Tiro Alto',
+            comentarios: [
+                {
+                    nombreUsuario: 'Juana',
+                    comentario: 'El Jean muy lindo puedo',
+                    imagen: '/images/users/2.jpeg',
+
+                },
+                {
+                    nombreUsuario: 'Ana',
+                    comentario: 'Me encanta, mi jean favorito',
+                    imagen: '/images/users/5.jpeg',
+                },
+                {
+                    nombreUsuario: 'Lara',
+                    comentario: 'Mi hermana me vió el jean y fue a comprarse el mismo',
+                    imagen: '/images/users/3.jpeg',
+
+                }
+            ]
+
+        },
+        {
+            Imagen: 'proyecto_integrador/proyecto/public/images/products/top.jpeg',
+            nombre: 'Top',
+            description: 'Top corto de escote pico cruzado y manga sisa.',
+            comentarios: [
+                {
+                    nombreUsuario: 'Juana',
+                    comentario: 'El top queda hermoso',
+                    imagen: '/images/users/2.jpeg',
+
+                },
+                {
+                    nombreUsuario: 'Ana',
+                    comentario: 'Muy elegante para salir',
+                    imagen: '/images/users/5.jpeg',
+                },
+                {
+                    nombreUsuario: 'Lara',
+                    comentario: 'Me gusta!',
+                    imagen: '/images/users/3.jpeg',
+
+                }
+            ]
+
+        },
+        {
+            Imagen: 'proyecto_integrador/proyecto/public/images/products/campera.jpeg',
+            nombre: 'Campera',
+            description: 'Cazadora corta de cuello solapa con manga larga.',
+            comentarios: [
+                {
+                    nombreUsuario: 'Matias',
+                    comentario: 'La campera que compré es increíblemente abrigada y cómoda!',
+                    imagen: '/images/users/4.avif',
+
+                },
+                {
+                    nombreUsuario: 'Ana',
+                    comentario: 'Perfecta para los días fríos de invierno.',
+                    imagen: '/images/users/5.jpeg',
+                },
+                {
+                    nombreUsuario: 'Luciana',
+                    comentario: '¡Qué buena calidad tiene la campera que elegí! Definitivamente vale la pena.',
+                    imagen: '/images/users/1.webp',
+
+                }
+            ]
+
+        },
+        {
+            Imagen: 'proyecto_integrador/proyecto/public/images/products/vestido.jpeg',
+            nombre: 'vestido',
+            description: 'Vestido corto de escote recto y hombros descubiertos.',
+            comentarios: [
+                {
+                    nombreUsuario: 'Juana',
+                    comentario: 'Mi nuevo vestido es elegante y me queda genial, estoy muy contenta con la compra',
+                    imagen: '/images/users/2.jpeg',
+
+                },
+                {
+                    nombreUsuario: 'Ana',
+                    comentario: 'El vestido que compré tiene un color precioso, me hace sentir muy femenina.',
+                    imagen: '/images/users/5.jpeg',
+                },
+                {
+                    nombreUsuario: 'Lara',
+                    comentario: 'Qué cómodo es mi vestido nuevo! Ideal para eventos formales y muy favorecedor.',
+                    imagen: '/images/users/3.jpeg',
+
+                }
+            ]
+
+        },
+        {
+            Imagen: 'proyecto_integrador/proyecto/public/images/products/zapatos.jpeg',
+            nombre: 'Zapatos',
+            description: 'Zapato destalonado de tacón kitten. Tira trasera con cierre mediante hebilla. Acabado en punta.',
+            comentarios: [
+                {
+                    nombreUsuario: 'Matias',
+                    comentario: 'Mis nuevos zapatos son muy cómodos, puedo usarlos todo el día sin problemas.',
+                    imagen: '/images/users/4.avif',
+
+                },
+                {
+                    nombreUsuario: 'Ana',
+                    comentario: 'El diseño de mis zapatos nuevos es muy moderno, estoy muy feliz con la elección..',
+                    imagen: '/images/users/5.jpeg',
+                },
+                {
+                    nombreUsuario: 'Luciana',
+                    comentario: 'Qué buen material tienen mis zapatos nuevos, se nota la calidad en cada paso.',
+                    imagen: '/images/users/1.webp',
+
+                }
+            ]
+
+        },
+        {
+            Imagen: 'proyecto_integrador/proyecto/public/images/products/pollera.jpeg',
+            nombre: 'Pollera',
+            description: 'Falda pantalón de tiro alto.',
+            comentarios: [
+                {
+                    nombreUsuario: 'Juana',
+                    comentario: 'La pollera que compré es muy versátil, la puedo combinar de muchas formas.',
+                    imagen: '/images/users/2.jpeg',
+
+                },
+                {
+                    nombreUsuario: 'Ana',
+                    comentario: 'Me gusta mucho el color de mi nueva pollera, es perfecto para el verano..',
+                    imagen: '/images/users/5.jpeg',
+                },
+                {
+                    nombreUsuario: 'Lara',
+                    comentario: 'Qué buen ajuste tiene mi pollera nueva, es cómoda y favorecedora.',
+                    imagen: '/images/users/3.jpeg',
+
+                }
+            ]
+
+        },
+        {
+            Imagen: 'proyecto_integrador/proyecto/public/images/products/blazer.jpeg',
+            nombre: 'Blazer',
+            description: 'Blazer abierta de cuello y solapa con manga larga acabada en vuelta y hombreras',
+            comentarios: [
+                {
+                    nombreUsuario: 'Matias',
+                    comentario: 'Mi nuevo blazer es elegante y perfecto para ocasiones formales, estoy encantada.',
+                    imagen: '/images/users/4.avif',
+
+                },
+                {
+                    nombreUsuario: 'Ana',
+                    comentario: 'Qué buen corte tiene mi blazer nuevo, se ajusta muy bien a mi figura.',
+                    imagen: '/images/users/5.jpeg',
+                },
+                {
+                    nombreUsuario: 'Luciana',
+                    comentario: 'La calidad de mi blazer nuevo es excelente, se nota que está bien hecho.',
+                    imagen: '/images/users/1.webp',
+
+                }
+            ]
+
+        },
+        {
+            Imagen: 'proyecto_integrador/proyecto/public/images/products/mono.jpeg',
+            nombre: 'Mono',
+            description: 'Mono largo de cuello redondo con manga sisa y manga corta.',
+            comentarios: [
+                {
+                    nombreUsuario: 'Juana',
+                    comentario: 'El enterito que compré es muy versátil, puedo usarlo en muchas ocasiones diferentes.',
+                    imagen: '/images/users/2.jpeg',
+
+                },
+                {
+                    nombreUsuario: 'Ana',
+                    comentario: 'Me gusta mucho mi enterito nuevo, es muy llamativo.',
+                    imagen: '/images/users/5.jpeg',
+                },
+                {
+                    nombreUsuario: 'Lara',
+                    comentario: 'Qué cómodo es mi enterito nuevo, es perfecto para estar cómoda y a la moda.',
+                    imagen: '/images/users/3.jpeg',
+
+                }
+            ]
+
         }
+        
     ]
 
 
 }
+
+module.exports = Data;
