c115d4d74bec2348a91bc7993dfae189ad3f98fa
julietaBdenevi <jburgosdenevi@udesa.edu.ar>
2024-04-22 22:57:48 -0300

    arreglo rutas e imagen

--- proyecto/controllers/productController.js
+++ proyecto/controllers/productController.js
@@ -3,7 +3,7 @@ const data = require("../db/data");
 const productController = {
 
     newProduct : function(req,res){
-        res.render('product-add', {title: 'Añadir producto', userName: data.usuario.userName});
+        res.render('productAdd', {title: 'Añadir producto', userName: data.usuario.userName});
 
     },
     product: function(req, res){
