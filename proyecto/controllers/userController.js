4af5d8390875ef6ac8cff39b9c3ce8823de309bb
julietaBdenevi <jburgosdenevi@udesa.edu.ar>
2024-06-10 17:07:19 -0300

    arreglo register

--- proyecto/controllers/userController.js
+++ proyecto/controllers/userController.js
@@ -1,31 +1,37 @@
-
 const db =require('../database/models');
 
 const userController = {
     register : function(req,res){
-        res.render('register',{title: 'Register'});
+       db.Usuario.create({
+        emailUsuario: req.body.mail,
+        nombreUsuario: req.body.user,
+        passUsuario: req.body.contrasenia,
+        nacimientoUsuario: req.body.fecha,
+        dniUsuario: req.body.documento,
+        fotoUsuario: req.body.fotoPerfil
+       })
 
     },
     registerInfo : function (req, res) {
         let registerForm = req.body;
-        db.usuario.create(registerForm)
+        db.Usuario.create(registerForm)
         .then(function (result) { // en vez de result va registerForm?
             return res.redirect ('/')     
         }).catch (error => console.log(error))
         
     },
-    login : function (req,res){
-        res.render('login', {title: 'login', usuario: data.usuario});
-    },
-    loginInfo: function (req,res){
-        let loginForm = req.body;
-        db.usuario.create(loginForm)
-        .then(function(result){  //en vez de result va loginForm?
-            return res.redirect ('/')
-        }).catch (error => console.log(error))
-        //lo redirecciona a la pag principal o la de profile?
+    // login : function (req,res){
+    //     res.render('login', {title: 'login', usuario: data.usuario});
+    // },
+    // loginInfo: function (req,res){
+    //     let loginForm = req.body;
+    //     db.Usuario.create(loginForm)
+    //     .then(function(result){  //en vez de result va loginForm?
+    //         return res.redirect ('/')
+    //     }).catch (error => console.log(error))
+    //     //lo redirecciona a la pag principal o la de profile?
 
-    }, 
+    // }, 
     
     profile: function(req,res){
         res.render ('profile',{title: 'Profile', data: data});
@@ -36,7 +42,7 @@ const userController = {
     },
     profileEditInfo: function(req,res) {
         let profileForm = req.body;
-        db.usuario.create(profileForm)
+        db.Usuario.create(profileForm)
         .then(function(result){  //en vez de result va profileForm?
             return res.redirect ('/')
         }).catch (error => console.log(error))
