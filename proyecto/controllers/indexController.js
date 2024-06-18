241498a7bcc4b95d3fc60f0bcaabca4c37cc0135
simonamatlis <smatlis@udesa.edu.ar>
2024-06-18 13:57:46 -0300

    arreglos

--- proyecto/controllers/indexController.js
+++ proyecto/controllers/indexController.js
@@ -1,8 +1,10 @@
 const db =require('../database/models');
 
 const indexController = {
-    index: function(req,res){   
-      res.render("index",{title: 'Sara', productos: db.producto} )
+    index: function(req,res){  
+      let productos= db.producto;
+      
+      res.render("index",{title: 'Sara', productos: productos} )
     },
     search: function(req, res){
         res.render('search-results', {title: 'Resultados', productos: data.producto, userName: userName});
