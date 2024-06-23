8a0323828762d9b86d5af6a5e003f21aba013875
simonamatlis <smatlis@udesa.edu.ar>
2024-06-23 13:27:23 -0300

    products

--- proyecto/controllers/productController.js
+++ proyecto/controllers/productController.js
@@ -1,17 +1,36 @@
 const db =require('../database/models');
+const producto = db.Producto;
+const comentarios = db.Comentarios;
 
 const productController = {
 
-    newProduct : function(req,res){
-        //debería poner db.usuario.id_usuario?
-        return res.render('productAdd', {title: 'Añadir producto', userName: data.usuario.userName});
 
-    },
     product: function(req, res){
+        let id_producto = req.params.id
+        producto.findByPk(id_producto, {
+            include: [ { association: "usuario"}, { association: "comentarios" , include: [{ association: "usuario"}]}]
+        })
+        .then((data)=>{
         //debería poner db.productos?
         res.render('product',{title: 'detalle del producto', producto: data.producto});
+        })
+        .catch((err) =>{console.log(err);})
 
     },
+
+
+    newProduct : function(req,res){
+        producto.findByPk(id, {
+            include: [ { association: "usuario"}, { association: "comentarios" , include: [{ association: "usuario"}]}]
+        })
+        .then((data)=>{
+            return res.render('productAdd', {title: 'Añadir producto', userName: data.usuario.userName});
+
+        })
+        //debería poner db.usuario.id_usuario?
+        
+    },
+    
     guardaInfo: function(req, res){
         //return res.send(req.body)
         let form = req.body;
